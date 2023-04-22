85f46f805faab57b624195ff2b4631ca05492863
Aryel Anne Louise de Souza Gabriel <69636549+Aryel15@users.noreply.github.com>
2023-04-22 16:09:43 -0300

    controle cadastro

--- src/components/Controle_Cadastros/Controle_Cadastros.jsx
+++ src/components/Controle_Cadastros/Controle_Cadastros.jsx
@@ -4,9 +4,9 @@ export default function Controle_Cadastros() {
     return (
         <>
             <div className="cadastros">
-                <div className="link"><a href="/cadastro-usuario"><i className="fa-solid fa-user"></i> Usuário</a><br /></div>
-                <div className="link ativado"><a href="/cadastro-ong"><i className="fa-solid fa-handshake-angle"></i>Ong's</a><br /></div>
-                <div className="link"><a href="/cadastro-moderador"><i className="fa-solid fa-gears"></i>Moderador</a><br /></div>
+                <div className={window.location.pathname == "/cadastro-usuario" ? "link ativado" : "link"}><a href="/cadastro-usuario"><i className="fa-solid fa-user"></i> Usuário</a><br /></div>
+                <div className={window.location.pathname == "/cadastro-ong" ? "link ativado" : "link"}><a href="/cadastro-ong"><i className="fa-solid fa-handshake-angle"></i>Ong's</a><br /></div>
+                <div className={window.location.pathname == "/cadastro-moderador" ? "link ativado" : "link"}><a href="/cadastro-moderador"><i className="fa-solid fa-gears"></i>Moderador</a><br /></div>
             </div>
         </>
     )
