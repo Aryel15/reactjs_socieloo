0cc1d9a2ce78e14d980c18727649abd0c9e099cf
Aryel Anne Louise de Souza Gabriel <69636549+Aryel15@users.noreply.github.com>
2023-05-03 21:41:52 -0300

    .

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -19,22 +19,23 @@ export default function Cadastro() {
     agencia: '',
     conta: '',
     pix: '',
+    imagens: '',
     descricao: '',
     segmento: ''
   })
   const pages = [
-    <Cadastrar step={step} setStep={setStep} setCadastro={setCadastro} cadastro={cadastro} />,
-    <ValidaEmail step={step} setStep={setStep} cadastro={cadastro} />,
+    <Cadastrar step={step} setStep={setStep} setCadastro={setCadastro} cadastro={cadastro}/>, 
+    <ValidaEmail step={step} setStep={setStep} cadastro={cadastro}/>, 
   ];
   return (
     <>
       <Menu />
-      {pages[step]}
+        {pages[step]}
     </>
   )
 }
 
-function Cadastrar({ step, setStep, cadastro, setCadastro }) {
+function Cadastrar({step, setStep, cadastro, setCadastro}){
   const [senha, setSenha] = useState("")
   const [cnae, setCnae] = useState("")
 
@@ -66,7 +67,6 @@ function Cadastrar({ step, setStep, cadastro, setCadastro }) {
     for (let i = 0; i < 5; i++) {
       codigo += Math.floor(Math.random() * 10);
     }
-    console.log(codigo);
     return codigo;
   }
 
@@ -74,52 +74,56 @@ function Cadastrar({ step, setStep, cadastro, setCadastro }) {
     e.preventDefault();
 
     let codigo = gerarCodigo();
-    Axios.get("https://servicodados.ibge.gov.br/api/v2/cnae/classes/" + cadastro.cnaeNumeros)
+    /*Axios.get("https://servicodados.ibge.gov.br/api/v2/cnae/classes/" + cadastro.cnae)
     .then((response) =>{
-      if(response.data.length > 0) {
+      if(typeof response.data === "object") {
         setCnae("");
-        console.log(response.data);
-      } else {
-        console.log(response.data);
-        setCnae("Cnae inválido");
-        return;
-      }
-    })
-    if (!senhaForte(cadastro.senha)) {
+        console.log("Válido:" + response.data);*/
+
+        if (!senhaForte(cadastro.senha)) {
           setSenha('Senha fraca');
           return;
-    } else {
+        } else {
           setSenha('');
           sessionStorage.setItem("codigo", codigo)
-
+    
           emailjs.init("user_J1drGGzW0lBRJmiE0X9Kg");
           emailjs.send("serviceID", "template_xwxib2d", {
             to_email: cadastro.email,
             codigo: codigo.toString(),
           }, "QSlqTSkhTipqcM7El")
-            .then((response) => {
-              console.log("SUCCESS!", response.status, response.text);
-              setStep(step + 1);
-            }, (err) => {
+          .then((response) => {
+            console.log("SUCCESS!", response.status, response.text);
+            setStep(step+1);
+          }, (err) => {
               console.log("FAILED...", err);
-          });
-     }
+          }).catch((error) => {console.log(error)});
+        }
+      /*} else {
+        console.log("Inválido:" + response.data);
+        setCnae("Cnae inválido");
+        return;
+      }
+    }).catch((error) => {console.log(error)});*/
   };
 
 
   return (
     <>
       <main className="cadastro">
+        <div className="formulario">
           <form className="formcad" id="login" action="javascript:void(0)" onSubmit={handleClickCadastro}>
             <div className="titulo-cad">
               <h1>Cadastre sua Ong</h1>
             </div>
-            <div className="form-cad">
+            <main className="form-cad">
               <section className="form__group">
                 <label for="nome">Nome:</label>
                 <input type="text" name="nome" id="nome" onChange={valorCadastro} required />
                 <label for="cnae">Cnae:</label>
-                <input type="text" name="cnae" id="cnae" onChange={valorCadastro} required />
+                {/* <IMaskInput mask="0000-0|00" name="cnae" id="cnae" onChange={valorCadastro} required/> */}
+                <input type="text" name="cnae" id="cnae" placeholder="0000-0|00" onChange={valorCadastro} required />
+                <p className="senha-fraca">{cnae}</p>
                 <label for="email">E-mail:</label>
                 <input type="email" name="email" id="email" onChange={valorCadastro} required />
                 <label for="telefone">Telefone</label>
@@ -130,35 +134,35 @@ function Cadastrar({ step, setStep, cadastro, setCadastro }) {
               </section>
 
               <section className="form__group">
-                <label for="regiao">Região</label>
-                <div id="group__select">
-                  <select name="regiao" id="regiao" onChange={valorCadastro} className="cad-select" required>
-                    <option value="#" selected disabled>Selecione uma opção</option>
-                    <option value="norte">Zona Norte</option>
-                    <option value="sul">Zona Sul</option>
-                    <option value="centro">Centro</option>
-                    <option value="leste">Zona Leste</option>
-                    <option value="oeste">Zona Oeste</option>
-                  </select>
-                </div>
-                <div className="banco">
-                  <div className="agencia">
-                    <label for="agencia">Agência</label>
-                    <input type="text" id="agencia" name="agencia" onChange={valorCadastro} required />
+                  <label for="regiao">Região</label>
+                  <div id="group__select">
+                    <select name="regiao" id="regiao" onChange={valorCadastro} className="cad-select" required>
+                      <option value="#" selected disabled>Selecione uma opção</option>
+                      <option value="norte">Zona Norte</option>
+                      <option value="sul">Zona Sul</option>
+                      <option value="centro">Centro</option>
+                      <option value="leste">Zona Leste</option>
+                      <option value="oeste">Zona Oeste</option>
+                    </select>
                   </div>
-                  <div className="conta">
-                    <label for="conta">Conta</label>
-                    <input type="text" id="conta" name="conta" onChange={valorCadastro} required />
+                  <div className="banco">
+                    <div className="agencia">
+                      <label for="agencia">Agência</label>
+                      <input type="text" id="agencia" name="agencia" onChange={valorCadastro} required />
+                    </div>
+                    <div className="conta">
+                      <label for="conta">Conta</label>
+                      <input type="text" id="conta" name="conta" onChange={valorCadastro} required />
+                    </div>
                   </div>
-                </div>
-                <label for="pix">Pix</label>
-                <input type="text" id="pix" name="pix" onChange={valorCadastro} required />
-                <label htmlFor="imagens">Imagens</label>
-                <input type="file" name="imagens" id="imagens" onChange={valorCadastro} />
+                  <label for="pix">Pix</label>
+                  <input type="text" id="pix" name="pix" onChange={valorCadastro} required />
+                  <label htmlFor="imagens">Imagens</label>
+                  <input type="file" name="imagens" id="imagens" onChange={valorCadastro} />
               </section>
 
               <section className="form__group">
-                <div className="group__about">
+              <div className="group__about">
                   <div className="about__radios">
                     <label className="radio-label">Qual a causa da sua ONG?</label>
                     <div className="radio__options">
@@ -196,52 +200,47 @@ function Cadastrar({ step, setStep, cadastro, setCadastro }) {
                   </div>
                 </div>
               </section>
-            </div>
+            </main>
             <label for="descricao">Descrição</label>
             <textarea name="descricao" id="descricao" cols="30" rows="10" onChange={valorCadastro} ></textarea>
             <button>Cadastre</button>
           </form>
+        </div>
       </main>
       <Controle_Cadastros />
       <Vlibras />
     </>
   );
 }
-function ValidaEmail({ cadastro }) {
-  const [msg, setMsg] = useState("")
-  const [codigo, setCodigo] = useState("")
+function ValidaEmail({cadastro}){
+    const [msg, setMsg] = useState("")
+    const [codigo, setCodigo] = useState("")
 
-  const handleClickCadastro = e => {
-    e.preventDefault()
-    const codigoArmazenado = sessionStorage.getItem("codigo");
-    if (codigo === codigoArmazenado) {
-      console.log(cadastro)
-      window.sessionStorage.removeItem("codigo")
-      Axios.post("http://localhost:8080/api/v1/ong", {
-        nome: cadastro.nome,
-        cnae: cadastro.cnae,
-        email: cadastro.email,
-        telefone: cadastro.telefone,
-        senha: cadastro.senha,
-        regiao: cadastro.regiao,
-        agencia: cadastro.agencia,
-        contaCorrente: cadastro.conta,
-        pix: cadastro.pix,
-        descricao: cadastro.descricao,
-        segmento: cadastro.segmento
-      }).then((response) => {
-        localStorage.setItem("id", response.data.id);
-        setMsg("✔ Sua Ong foi cadastrada")
-        setTimeout(() => {
-          window.location.pathname = "/perfil-ong"
-        }, 3000);
-      }).catch((err) => console.log(err))
-    } else {
-      alert("Código inválido");
+    const handleClickCadastro = e =>{
+      e.preventDefault()
+      const codigoArmazenado = sessionStorage.getItem("codigo");
+      if (codigo === codigoArmazenado) {
+        console.log(cadastro)
+        window.sessionStorage.removeItem("codigo")
+        Axios.post("http://localhost:8080/api/v1/ong", {
+          nome: cadastro.nome,
+          cnae: cadastro.cnae,
+          email: cadastro.email,
+          telefone: cadastro.telefone,
+          senha: cadastro.senha
+        }).then((response) => {
+          localStorage.setItem("id", response.data.id);
+          setMsg("✔ Sua Ong foi cadastrada")
+          setTimeout(() => {
+            window.location.pathname = "/perfil-ong"
+          }, 3000); 
+        })
+      }else{
+        alert("Código inválido");
+      }
     }
-  }
 
-  return (
+    return (
     <>
       <main className="cadastro">
         <div className="formulario">
