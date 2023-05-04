4d249b58fb4e2be7e50624efc4be0ccaa697cb17
Aryel Anne Louise de Souza Gabriel <69636549+Aryel15@users.noreply.github.com>
2023-05-04 16:11:54 -0300

    Filtro Home

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import './style.css'
 
 export default function Card(props) {
-    
+
 
     return (
         <>
             <a href={props.link}>
                 <div className="card">
                     <div className="img pa">
-                        <img src={props.icon} alt="" />
+                        <img src={`../../../imgs/icons/${props.segmento}.png`} alt="" />
                     </div>
                     <h2>{props.titulo}</h2>
                     <div className="btns_card">
-                        <a href={props.categorias} className="button">{props.categoria}</a>
-                        <a href={props.regioes} className="button">{props.regiao}</a>
+                        <a href={props.segmento} className="button">{props.segmento}</a>
+                        <a href={props.regiao} className="button">{props.regiao}</a>
                     </div>
                 </div>
             </a>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,13 +7,22 @@ import Axios from 'axios';
 
 export default function Home() {
   const [data, setData] = useState([])
-  const [filter, setFilter] = useState("todas")
+  const [filter, setFilter] = useState({
+    regiao: "",
+    segmento: "",
+  })
+  const [regiao, setRegiao] = useState("todas")
+  const [segmento, setSegmento] = useState("todas")
   useEffect(() =>{
     Axios.get("http://localhost:8080/api/v1/ong")
     .then((response) =>{
         setData(response.data);
     })
-},[])
+  },[])
+  function handleClickSearch(){
+    setRegiao(filter.regiao);
+    setSegmento(filter.segmento);
+  }
 
   return (
     <>
@@ -24,7 +33,7 @@ export default function Home() {
               <h1 id="title-index">Um ato de bondade pode ajudar muita gente!</h1>
               <br/>
               <div className="buscador">
-                  <select name="Segmentos" id="segmentos">
+                  <select name="Segmentos" id="segmentos" onChange={(e) => setFilter({...filter, segmento:e.target.value})}>
                       <option value="Cidadania">Cidadania</option>
                       <option value="Cultura e Esportes">Cultura e Esportes</option>
                       <option value="Educação">Educação</option>
@@ -32,18 +41,20 @@ export default function Home() {
                       <option value="Meio Ambiente">Meio Ambiente</option>
                       <option value="Proteção Animal">Proteção Animal</option>
                       <option value="Saúde">Saúde</option>
+                      <option value="Todas">Todos</option>
                       <option value="Segmentos" selected disabled>Segmentos</option>
                   </select>
-                  <select name="Região" id="Região">
+                  <select name="Região" id="Região" onChange={(e) => setFilter({...filter, regiao:e.target.value})}>
                       <option value="Zona Leste">Zona Leste</option>
                       <option value="Zona Norte">Zona Norte</option>
                       <option value="Centro">Centro</option>
                       <option value="Zona Oeste">Zona Oeste</option>
                       <option value="Zona Sul">Zona Sul</option>
+                      <option value="Todas">Todas</option>
                       <option value="Região" selected disabled>Região</option>
                   </select>
                   <div className="search">
-                      <a href="#">
+                      <a href="javascript:void(0)" onClick={handleClickSearch}>
                         <i className="fa-solid fa-magnifying-glass"></i>
                       </a>
                   </div>
@@ -53,8 +64,8 @@ export default function Home() {
       <section id="main-index">
         <div className="section__cards">
         {
-          data.map(ong => (
-            <Card categoria="Proteção Animal" titulo={ong.nome} icon='./imgs/cil_animal.png' regiao={ong.regiao} segmento={ong.segmento} link=""/>
+          data.filter((ong) => (regiao === 'Todas' && segmento === 'Todas') || (regiao === ong.regiao && segmento === ong.segmento) ||  (regiao === ong.regiao && segmento === 'Todas') || (regiao === 'Todas' && segmento === ong.segmento)).map(ong => (
+            <Card categoria="Proteção Animal" titulo={ong.nome} regiao={ong.regiao} segmento={ong.segmento} link=""/>
           ))
         }
 
