4195bbddee024efbe4b13a7706d3f17c3daec98d
Aryel15 <aannelouise@outlook.com>
2023-05-08 11:21:22 -0300

    Página ong

--- src/pages/Ong/Ong.jsx
+++ src/pages/Ong/Ong.jsx
@@ -23,31 +23,58 @@ export default function Ong() {
                 console.log(err);
             })
     }, [])
+    const [step, setStep] = React.useState("Editar_Perfil");
+    const pages = {
+        Ong: <OngInfo ong={ong} step={step} setStep={setStep} data={data} id={id} />,
+        Comentarios: <Comentarios step={step} setStep={setStep} data={data} id={id}  />,
+        Avaliar: <Avaliar step={step} setStep={setStep} data={data} id={id}  />,
+    }
 
     return (
         <>
             <Menu />
-            {/* Carrossel*/}
-            <section className="section__ong">
-                <section className="ong__imagens">
-
-                    <ul id="imagens__container">
-                        <li className="container__item">
-                            <img src="../imgs/fotosOng/fotos01.jpg" alt="Imagem da ong mostrando os integrantes" />
-                        </li>
-
-                        <li className="container__item">
-                            <img src="../imgs/fotosOng/fotos02.jpg" alt="Imagem de um cachorro muito engraçado" />
-                        </li>
-
-                        <li className="container__item">
-                            <img src="../imgs/fotosOng/fotos03.jpg" alt="Localização da Ong no Google Maps" />
-                        </li>
+            <main className="OngPage">
+                {/* Carrossel*/}
+                <div className="options__photos">
+                    <img src="../imgs/fotosOng/fotos01.jpg" alt="foto de perfil escolhida pela ong" />
+                    <h1>{data?.nome}</h1>
+                </div>
+                <aside className="edit__options">
+                    <ul className="options__itens">
+                        <li className={step === "Ong" ? "select" : ""}><a href="javascript:void(0);" className="options__item" onClick={() => setStep("Ong")}><i className='bx bx-pencil'></i> Ong</a></li>
+                        <li className={step === "Comentarios" ? "select" : ""}><a href="javascript:void(0);" className="options__item" onClick={() => setStep("Comentarios")}><i className='bx bxs-lock-alt'></i> Comentarios</a></li>
+                        <li className={step === "Avaliar" ? "select" : ""}><a href="javascript:void(0);" className="options__item" onClick={() => setStep("Avaliar")}><i className='bx bxs-lock-alt'></i> Avaliar</a></li>
                     </ul>
+                </aside>
+                <section className="section__ong">
+                    <section className="ong__imagens">
 
+                        <ul id="imagens__container">
+                            <li className="container__item">
+                                <img src="../imgs/fotosOng/fotos01.jpg" alt="Imagem da ong mostrando os integrantes" />
+                            </li>
+
+                            <li className="container__item">
+                                <img src="../imgs/fotosOng/fotos02.jpg" alt="Imagem de um cachorro muito engraçado" />
+                            </li>
+
+                            <li className="container__item">
+                                <img src="../imgs/fotosOng/fotos03.jpg" alt="Localização da Ong no Google Maps" />
+                            </li>
+                        </ul>
+
+                    </section>
+                    {pages[step]}
+                    
                 </section>
-
-                <section className="ong__informations">
+            </main>
+            <Vlibras />
+        </>
+    )
+}
+function OngInfo({ ong, step, setStep, data, id }){
+    return(
+        <section className="ong__informations">
                     <div className="informations__description">
                         <h1>{ong ? data?.nome : "Adote Sempre Cabe Mais Um - Zona Leste"}</h1>
 
@@ -79,37 +106,133 @@ export default function Ong() {
                                     <p>{ong ? data?.pix : "adotemaisumPix@gmail.com"}</p>
                                 </div>
                             </div>
-
                         </div>
-
-
                     </div>
 
 
                     <div className="informations__botons">
-                        <div className="informations__comments">
-                            <textarea name="comments" id="comments" cols="100" rows="6" placeholder="Comente o que achou..."></textarea>
-
                             <div className="buttons__container">
-                                <button>
-                                    <p>Enviar</p>
-                                </button>
                                 <button>
                                     <p>Compartilhar</p> <img src="../assets/share.svg" alt="instagram icon" />
                                 </button>
                             </div>
                         </div>
-                    </div>
                 </section>
-            </section>
-
-            <section className="container__subtle">
-                <h2 id="subTitle">Sugestões</h2>
-            </section>
-
-            <section className="section__cards">
-            </section>
-            <Vlibras />
-        </>
     )
 }
+function Comentarios({ step, setStep, data, id }){
+    return(
+        <section className="secao_coments">
+            <div className="informations__description">
+
+                <div id="coment_usuario">         
+                    <img src="../imgs/user.png" alt="Ícone de usuário" id="img-feed"/>  
+                    <div className="texto">
+                        <h4>@User_logado</h4>
+                        <p><i>Seu comentário</i></p>
+                        <div className="pos_editbotao">
+                            <button className="editbotao">Editar comentário</button>
+                            <button className="editbotao">Deletar comentário</button>
+                        </div><br/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
+            
+                <div className="coments">
+                    <img src="../imgs/user.png" alt="Ícone de usuário" id="img-feed"/>
+                    <div className="texto">
+                        <h4>@User</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
+                    
+                <div className="coments">
+                    <div>
+                        <img src="../imgs/user.png" alt="Ícone de usuário" id="img-feed"/>
+                    </div>
+                    <div className="texto">
+                        <h4>@User</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
+
+                <div className="coments">
+                    <div>
+                        <img src="../imgs/user.png" alt="Ícone de usuário" id="img-feed"/>
+                    </div>
+                    <div className="texto">
+                        <h4>@User</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
+
+                <div className="coments">
+                    <div>
+                        <img src="../imgs/user.png" alt="Ícone de usuário" id="img-feed"/>
+                    </div>
+                    <div className="texto">
+                        <h4>@User</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
+
+                <div className="informations__comments">
+                    <textarea name="comments" id="comments" cols="100" rows="6" placeholder="Comente o que achou..."></textarea>
+                    <button className="botao">Enviar</button>
+                </div>
+            </div>
+        </section>
+    )
+}
+function Avaliar({ step, setStep, data, id }) {
+    return (
+      <>
+        <main className="main_content container">
+          <section className="section-seu-codigo container">
+              <div className="content">
+                  <div id="box-artigo" className="box-artigo">
+                      <div className="container_form">
+                          <h1>Avaliação das ONGs</h1>
+                          <form className="form" action="#" method="post">
+                              <div className="form_grupo">
+                                  <label for="nome" className="form_label">Avalie uma ONG de sua preferência</label>
+                                  <input type="text" name="nome" className="form_input" id="nome"
+                                      placeholder="Escreva o nome da ONG" required/>
+                              </div>
+  
+                              <div className="avaliacao__container">
+                                  <ul className="avaliacao">
+                                      <li className="star-icon ativo" data-avaliacao="1"></li>
+                                      <li className="star-icon" data-avaliacao="2"></li>
+                                      <li className="star-icon" data-avaliacao="3"></li>
+                                      <li className="star-icon" data-avaliacao="4"></li>
+                                      <li className="star-icon" data-avaliacao="5"></li>
+                                  </ul>
+                              </div>
+  
+                              <div className="form_grupo">
+                                  <div className="form_message">
+                                      <label for="message" className="form_message_label"> Escreva sua revisão aqui:</label>
+                                      <textarea name="mensagem" id="message" cols="30" rows="3"
+                                          className="form_input message_input" required></textarea>
+                                  </div>
+                                  <div className="options__checkbox">
+                                      <input type="checkbox" name="termos" id="termos"/>
+                                      <label className="check_label" for="termos">Eu concordo com os <a href="#">termos e
+                                              condições</a></label>
+                                  </div>
+                                  <div className="submit">
+                                      <input type="hidden" name="acao" value="enviar"/>
+                                      <button type="submit" name="Submit" className="submit_btn">Enviar</button>
+                                  </div>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+                  <div className="clear"></div>
+              </div>
+          </section>
+        </main>
+      </>
+    )
+  }
