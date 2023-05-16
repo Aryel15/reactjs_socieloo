c043dcc4ee933f702193ddc3f08c83532b608ddf
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2023-05-15 22:10:56 -0300

    Permissao de localizacao
    Filtra automaticamente as ONGs se o usuario permitir o acesso a sua localidade

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
 import Vlibras from '../../components/Vlibras/Vlibras';
 import Menu from '../../components/Menu/Menu';
@@ -13,14 +13,16 @@ export default function Home() {
   })
   const [regiao, setRegiao] = useState("Todas")
   const [segmento, setSegmento] = useState("Todas")
-  useEffect(() =>{
+
+  useEffect(() => {
     handleClickSearch();
     Axios.get("http://localhost:8080/api/v1/ong")
-    .then((response) =>{
+      .then((response) => {
         setData(response.data);
-    })
-  },[regiao, segmento])
-  function handleClickSearch(){
+      })
+  }, [regiao, segmento])
+
+  function handleClickSearch() {
     if (filter.regiao !== regiao || filter.segmento !== segmento) {
       setRegiao(filter.regiao);
       setSegmento(filter.segmento);
@@ -29,9 +31,51 @@ export default function Home() {
       setSegmento("Todas");
     }
   }
+
   function NomeOngLink(nome) {
     return nome.toLowerCase().replace(/\s+/g, '-');
   }
+
+  function setRegiaoPeloCep(cep) {
+    if (cep >= 1000000 && cep <= 1599999) {
+      return "Centro"
+    } else if (cep >= 2000000 && cep <= 2999999) {
+      return "Zona Norte"
+    } else if ((cep >= 3000000 && cep <= 3999999) || (cep >= 8000000 && cep <= 8499999)) {
+      return "Zona Leste"
+    } else if (cep >= 4000000 && cep <= 4999999) {
+      return "Zona Sul"
+    } else if (cep >= 5000000 && cep <= 5899999) {
+      return "Zona Oeste"
+    } else {
+      return null
+    }
+  }
+
+  useEffect(_ => {
+    navigator.geolocation.getCurrentPosition(async sucesso => {
+      localStorage.setItem("lat", sucesso.coords.latitude);
+      localStorage.setItem("long", sucesso.coords.longitude);
+      await Axios.get(`https://nominatim.openstreetmap.org/search.php?q=${localStorage.getItem("lat")}%2C${localStorage.getItem("long")}&format=jsonv2`)
+        .then((response) => {
+          if (response.data[0].length != 0) {
+            let searchCepPoint = response.data[0].display_name.search("-")
+            let getCepFromAPI = response.data[0].display_name.slice(searchCepPoint - 4, searchCepPoint + 4).replace("-", "")
+            let regiaoDefinidaPeloLocalidade = setRegiaoPeloCep(parseInt(getCepFromAPI));
+
+            if (regiaoDefinidaPeloLocalidade != null) {
+              setFilter({ ...filter, regiao: regiaoDefinidaPeloLocalidade })
+              setRegiao(regiaoDefinidaPeloLocalidade);
+            }
+          }
+        })
+    }, erro => {
+      console.log(erro);
+      localStorage.setItem("lat", null);
+      localStorage.setItem("long", null);
+    })
+  }, [])
+
 
   return (
     <>
@@ -39,52 +83,51 @@ export default function Home() {
       <main className="main_home">
         <section className="principal">
           <div className="busca">
-              <h1 id="title-index">Um ato de bondade pode ajudar muita gente!</h1>
-              <br/>
-              <div className="buscador">
-                  <select name="Segmentos" id="segmentos" onChange={(e) => setFilter({...filter, segmento:e.target.value})}>
-                      <option value="Cidadania">Cidadania</option>
-                      <option value="Cultura e Esportes">Cultura e Esportes</option>
-                      <option value="Educação">Educação</option>
-                      <option value="Gênero e Diversidade">Gênero e Diversidade</option>
-                      <option value="Meio Ambiente">Meio Ambiente</option>
-                      <option value="Proteção Animal">Proteção Animal</option>
-                      <option value="Saúde">Saúde</option>
-                      <option value="Todas">Todos</option>
-                      <option value="Segmentos" selected disabled>Segmentos</option>
-                  </select>
-                  <select name="Região" id="Região" onChange={(e) => setFilter({...filter, regiao:e.target.value})}>
-                      <option value="Zona Leste">Zona Leste</option>
-                      <option value="Zona Norte">Zona Norte</option>
-                      <option value="Centro">Centro</option>
-                      <option value="Zona Oeste">Zona Oeste</option>
-                      <option value="Zona Sul">Zona Sul</option>
-                      <option value="Todas">Todas</option>
-                      <option value="Região" selected disabled>Região</option>
-                  </select>
-                  <div className="search">
-                      <a href="javascript:void(0)" onClick={handleClickSearch}>
-                        <i className="fa-solid fa-magnifying-glass"></i>
-                      </a>
-                  </div>
+            <h1 id="title-index">Um ato de bondade pode ajudar muita gente!</h1>
+            <br />
+            <div className="buscador">
+              <select name="Segmentos" id="segmentos" onChange={(e) => setFilter({ ...filter, segmento: e.target.value })}>
+                <option value="Cidadania">Cidadania</option>
+                <option value="Cultura e Esportes">Cultura e Esportes</option>
+                <option value="Educação">Educação</option>
+                <option value="Gênero e Diversidade">Gênero e Diversidade</option>
+                <option value="Meio Ambiente">Meio Ambiente</option>
+                <option value="Proteção Animal">Proteção Animal</option>
+                <option value="Saúde">Saúde</option>
+                <option value="Todas">Todos</option>
+                <option value="Segmentos" selected disabled>Segmentos</option>
+              </select>
+              <select name="Região" id="Região" onChange={(e) => setFilter({ ...filter, regiao: e.target.value })} value={filter.regiao}>
+                <option value="Zona Leste">Zona Leste</option>
+                <option value="Zona Norte">Zona Norte</option>
+                <option value="Centro">Centro</option>
+                <option value="Zona Oeste">Zona Oeste</option>
+                <option value="Zona Sul">Zona Sul</option>
+                <option value="Todas">Todas</option>
+                <option value="Região" selected disabled>Região</option>
+              </select>
+              <div className="search">
+                <a href="javascript:void(0)" onClick={handleClickSearch}>
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </a>
               </div>
+            </div>
           </div>
-      </section>
-      <section id="main-index">
-        <div className="section__cards">
-        {
-          data.filter(
-            ong =>
-              (regiao === 'Todas' || ong.regiao.toLowerCase() === regiao.toLowerCase()) &&
-              (segmento === 'Todas' || ong.segmento.toLowerCase() === segmento.toLowerCase())
-          ).map(ong => (
-            <Card categoria="Proteção Animal" titulo={ong.nome} regiao={ong.regiao} segmento={ong.segmento} link={`/ong/${ong.id}/`} id={ong.id}/>
-          ))
-        }
-
-        </div>
-        <Vlibras />
-      </section>
+        </section>
+        <section id="main-index">
+          <div className="section__cards">
+            {
+              data.filter(
+                ong =>
+                  (regiao === 'Todas' || ong.regiao.toLowerCase() === regiao.toLowerCase()) &&
+                  (segmento === 'Todas' || ong.segmento.toLowerCase() === segmento.toLowerCase())
+              ).map(ong => (
+                <Card categoria="Proteção Animal" titulo={ong.nome} regiao={ong.regiao} segmento={ong.segmento} link={`/ong/${ong.id}/`} id={ong.id} />
+              ))
+            }
+          </div>
+          <Vlibras />
+        </section>
       </main>
     </>
   )
